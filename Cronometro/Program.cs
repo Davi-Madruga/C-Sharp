e7d832ea7e1907b422eff3f36004e217332d41a8
F3IT3 <davimadruga@outlook.com.br>
2025-10-08 16:08:44 -0300

    Feat: Cronometro(ritmo do clock CPU)

--- Cronometro/Program.cs
+++ Cronometro/Program.cs
@@ -6,7 +6,22 @@ namespace Cronometro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Start();
+        }
+        static void Start()
+        {
+                        Console.Clear();
+            int tempoInicial = int.Parse(Console.ReadLine());
+            int tempo = 0;
+            while (tempo <= tempoInicial)
+            {
+                Console.Clear();
+                int hora = tempo / 3600;
+                int minuto = tempo % 3600 / 60;
+                int segundo = tempo % 60;
+                Console.WriteLine($"{hora}:{minuto}:{segundo}");
+                tempo++;
+            }
         }
     }
 }
