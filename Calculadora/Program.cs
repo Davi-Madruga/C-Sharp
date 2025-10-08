a15702ad85daf819d8f9f0d040c7923b5f323665
F3IT3 <davimadruga@outlook.com.br>
2025-10-08 14:18:48 -0300

    Feat Função Menu

---
+++ Calculadora/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static void Menu()
+        {
+            Console.WriteLine("---CALCULATOR---");
+            Console.WriteLine("1 - Somar");
+            Console.WriteLine("2 - Subtrair");
+            Console.WriteLine("3 - Multiplicar");
+            Console.WriteLine("4 - Dividir");
+            Console.WriteLine("5 - Sair");
+            short escolha = short.Parse(Console.ReadLine());
+            switch (escolha)
+            {
+                case 1:
+                    Somar();
+                    break;
+                case 2:
+                    Subtrair();
+                    break;
+                case 3:
+                    Multiplicar();
+                    break;
+                case 4:
+                    Dividir();
+                    break;
+                case 5:
+                    Sair();
+                    break;
+                default:
+                    Menu();
+                    break;
+            }
+        }
+        static void Somar()
+        {
+            
+        }
+        static void Subtrair()
+        {
+            
+        }
+        static void Multiplicar()
+        {
+            
+        }
+        static void Dividir()
+        {
+            
+        }
+        static void Sair()
+        {
+            
+        }
+    }
+}
