f6508dbbd98637bfb1cdd5c24fdef5370a2d1529
F3IT3 <davimadruga@outlook.com.br>
2025-10-09 11:03:26 -0300

    Editor de Textos

---
+++ EditorTexto/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+
+namespace EditorTexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("0 - Sair");
+            Console.WriteLine("1 - Abrir Arquivo");
+            Console.WriteLine("2 - Criar novo Arquivo");
+            short escolha = short.Parse(Console.ReadLine());
+            switch (escolha)
+            {
+                case 0:
+                    Environment.Exit(0);
+                    break;
+                case 1:
+                    Abrir();
+                    break;
+                case 2:
+                    Editar();
+                    break;
+                default:
+                    Menu();
+                    break;
+            }
+        }
+        static void Abrir()
+        {
+
+        }
+        static void Editar()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo: (ESC) para sair");
+            Console.WriteLine("------------------------");
+            string text = "";
+            do
+            {
+                text += "\n" + Console.ReadLine();
+                // text += Environment.NewLine; 
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+            Salvar(text);
+
+        }
+        static void Salvar(string text)
+        {
+            Console.Clear();
+            Console.WriteLine("Digite o caminho para salvar seu arquivo:");
+            var path = Console.ReadLine();
+
+            using (var file = new StreamWriter(path))
+            {
+                file.Write(text);
+            }
+            Console.WriteLine($"Arquivo {path} Salvo");
+            Console.ReadLine();
+            Menu();
+        }
+    }
+}
