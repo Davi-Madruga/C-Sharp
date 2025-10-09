f7851ec52ff68542a6ccf207701a7f83508c7c4b
F3IT3 <davimadruga@outlook.com.br>
2025-10-09 20:23:10 -0300

    Feat

--- Tempo/Program.cs
+++ Tempo/Program.cs
@@ -7,8 +7,22 @@ namespace Tempo
         static void Main(string[] args)
         {
             Console.Clear();
-            var data = new DateTime();
-            Console.WriteLine(data);
+            // var data = new DateTime(2025, 10, 10, 19, 03, 45);
+            // var formatada = String.Format("{0:dd/MM/yyyy}",data);
+            // // var data = DateTime.Now;
+            // Console.WriteLine(formatada);
+            // var utcDate = DateTime.UtcNow;
+            // Console.WriteLine(DateTime.Now);
+            // Console.WriteLine(utcDate);
+            // Console.WriteLine(utcDate.ToLocalTime());
+            var timeSpan = new TimeSpan();
+            Console.WriteLine(timeSpan);    
+            var timeSpansec = new TimeSpan(1222);
+            Console.WriteLine(timeSpansec);    
+            var timeSpans = new TimeSpan(13,24,23,1222,12323);
+            Console.WriteLine(timeSpans);    
+            
+            
         }
     }
 }
